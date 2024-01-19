e69ae9a79565c2c53db95072f38b1f56913c732a
Charles Saluski <charles.saluski@nau.edu>
2024-01-19 13:57:55 -0700

    Add new GPS method where location data is pulled from GPS data of another timestampted photo set

---
+++ dji_utils.py
@@ -0,0 +1,31 @@
+import os
+
+import exiftool
+import pandas as pd
+
+def gps_table_from_images(source_path):
+  images = os.listdir(source_path)
+  images = [os.path.join(source_path, image) for image in images]
+  
+  exif = exiftool.ExifToolHelper()
+  meta = exif.get_metadata(images)
+
+  original_cols = ['SourceFile', 'EXIF:DateTimeOriginal', 'EXIF:GPSLatitude', 'EXIF:GPSLongitude', 'EXIF:GPSAltitude']
+
+  rename_cols = ['filename', 'datetime', 'latitude', 'longitude', 'altitude']
+
+  ew_mult = {'E': 1, 'W':-1}
+  ns_mult = {'N': 1, 'S':-1}
+
+  df = pd.DataFrame(meta)
+  # correct sign of GPS decimal
+  df['EXIF:GPSLatitude'] = df['EXIF:GPSLatitude'] * df['EXIF:GPSLatitudeRef'].map(ns_mult)
+  df['EXIF:GPSLongitude'] = df['EXIF:GPSLongitude'] * df['EXIF:GPSLongitudeRef'].map(ew_mult)
+  # take only cols we want
+  df = df[original_cols]
+  # rename
+  df.columns = rename_cols
+  # convert datetime column from string to datetime
+  # DJI uses weird format, 2024:01:18 20:08:10, no timezone.
+  df['datetime'] = pd.to_datetime(df['datetime'], format="%Y:%m:%d %H:%M:%S")
+  return df
--- process.py
+++ process.py
@@ -1,15 +1,20 @@
-
+# Python builtin libraries
 import argparse
+import datetime
 import os
 import pathlib
 import re
 from multiprocessing import Pool
 from functools import partial
 
+# External libraries
 import numpy as np
 import tqdm
 from flirimageextractor import FlirImageExtractor
 from osgeo import gdal, osr
+
+# Local modules
+import dji_utils
 
 def setup_dirs(input_dir, output_dir, output_to_non_empty = False):
     try: 
@@ -74,17 +79,39 @@ def get_height(height_source, metadata):
     if height_source['type'] == 'height':
         return height_source['val']
     elif height_source['type'] == 'elevation':
-        gps_alt = float(re.search('(\d*.?\d*)',metadata['GPSAltitude']).group(1))
+        if type(metadata['GPSAltitude']) == str:
+            gps_alt = float(re.search('(\d*.?\d*)',metadata['GPSAltitude']).group(1))
+        elif type(metadata['GPSAltitude']) == np.float64:
+            gps_alt = metadata['GPSAltitude']
         ground_elevation = height_source['val']
         return gps_alt - ground_elevation
     elif height_source['type'] == 'elevation_map':
-        gps_lat = gps_str_to_dd(metadata['GPSLatitude'])
-        gps_long = gps_str_to_dd(metadata['GPSLongitude'])
-        gps_alt = float(re.search('(\d*.?\d*)',metadata['GPSAltitude']).group(1))
+        if type(metadata['GPSLatitude']) == str:
+            gps_lat = gps_str_to_dd(metadata['GPSLatitude'])
+            gps_long = gps_str_to_dd(metadata['GPSLongitude'])
+            gps_alt = float(re.search('(\d*.?\d*)',metadata['GPSAltitude']).group(1))
+        elif type(metadata['GPSLatitude']) == np.float64:
+            gps_lat = metadata['GPSLatitude']
+            gps_long = metadata['GPSLongitude']
+            gps_alt = metadata['GPSAltitude']
         ground_elevation = get_heightmap_elevation(height_source['val'], gps_lat, gps_long)
         return gps_alt - ground_elevation
 
-def process_flir_image(input_dir, output_dir, height_source, filename):
+def gps_from_source(metadata, gps_source):
+    # get DateTimeOriginal from metadata and convert using format="%Y:%m:%d %H:%M:%S"
+    # The Pro camera puts a decimal second and time zone on the DateTimeOriginal
+    # string, it is always 10 characters long. Looks like "".324-07:00".
+    # strptime doesn't have any nice way to tell it to ignore it, so we slice it off.
+    timestamp = metadata['DateTimeOriginal'][:-10]
+    timestamp = datetime.datetime.strptime(timestamp, "%Y:%m:%d %H:%M:%S")
+    
+    # GPS location data is not available every second, look up the closest matching time
+    gps = gps_source.loc[(gps_source['datetime'] - timestamp).abs().idxmin()]
+    metadata['GPSLatitude'] = gps['latitude']
+    metadata['GPSLongitude'] = gps['longitude']
+    metadata['GPSAltitude'] = gps['altitude']
+
+def process_flir_image(input_dir, output_dir, height_source, gps_source, filename):
     input_path = os.path.join(input_dir, filename)
     # Open the FLIR image using FlirImageExtractor
     flir_image = FlirImageExtractor()
@@ -92,7 +119,13 @@ def process_flir_image(input_dir, output_dir, height_source, filename):
     flir_image.fix_endian = False
     metadata = flir_image.get_metadata(input_path)
 
-    flir_image.default_distance = get_height(height_source, metadata)
+    if gps_source is not None:
+        gps_from_source(metadata, gps_source)
+
+    # GPS may not always give heights that are above ground reference altitude
+    # but obviously distance is always positive and is required for calculations
+    # so default to 1 meter
+    flir_image.default_distance = max(1, get_height(height_source, metadata))
     metadata['ObjectDistance'] = flir_image.default_distance
 
     # Get the visible and thermal images only when they exist
@@ -111,7 +144,7 @@ def process_flir_image(input_dir, output_dir, height_source, filename):
 
     if thermal_image is not None:
         # Save the thermal image as a GeoTIFF with EXIF metadata
-        save_geotiff(os.path.join(output_dir, file_name_without_extension + '_thermal.tif'), thermal_image, metadata)
+        save_geotiff(os.path.join(output_dir, file_name_without_extension + '_thermal.tif'), thermal_image, 'GTiff', metadata)
 
     # Check if the visible image is available before saving
     if visible_image is not None:
@@ -119,17 +152,24 @@ def process_flir_image(input_dir, output_dir, height_source, filename):
         # For example, you can apply filters, resize, etc.
 
         # Save the visible image as a GeoTIFF with EXIF metadata
-        save_geotiff(os.path.join(output_dir, file_name_without_extension + '_visible.tif'), visible_image, metadata)
+        save_geotiff(os.path.join(output_dir, file_name_without_extension + '_visible.jpg'), visible_image, 'JPEG', metadata)
 
-def save_geotiff(output_path, image_data, metadata):
+
+def save_geotiff(output_path, image_data, drivertype, metadata):
     # Create a GeoTIFF file
-    driver: gdal.Driver = gdal.GetDriverByName('GTiff')
+    driver: gdal.Driver = gdal.GetDriverByName(drivertype)
 
     # Thermal will have 1 band for temperature, visible will have 3 for RGB
     # normalize to 3 dimension array so that everything else is simpler.
     if image_data.ndim == 2:
         image_data = np.expand_dims(image_data, axis=2)
-    dataset: gdal.Dataset = driver.Create(output_path, image_data.shape[1], image_data.shape[0], image_data.shape[2], gdal.GDT_Float32)
+    if drivertype == 'JPEG':
+        # JPEG doesn't have a direct create method, it has to be copied from another dataset.
+        mem_driver = gdal.GetDriverByName( 'MEM' )
+        dataset = mem_driver.Create( '', image_data.shape[1], image_data.shape[0], image_data.shape[2], gdal.GDT_Byte)
+
+    if drivertype == 'GTiff':
+        dataset: gdal.Dataset = driver.Create(output_path, image_data.shape[1], image_data.shape[0], image_data.shape[2], gdal.GDT_Float32)
 
     # Set the spatial reference
     spatial_ref = osr.SpatialReference()
@@ -150,9 +190,18 @@ def save_geotiff(output_path, image_data, metadata):
     for key, value in metadata.items():
         dataset.SetMetadataItem(key, str(value))
 
+    if drivertype == 'JPEG':
+        # replace dataset reference from memory to jpeg driver
+        dataset = driver.CreateCopy(output_path, dataset, 0)
     # Close the dataset
     dataset.FlushCache()
     dataset = None
+
+def load_gps_table(gps_source_path, source_type):
+    if source_type == "dji":
+        return dji_utils.gps_table_from_images(gps_source_path)
+    else:
+        raise NotImplementedError
 
 if __name__ == "__main__":
 
@@ -166,7 +215,10 @@ if __name__ == "__main__":
                         help="Automatic yes to prompts, including clobbering output directory.")
     parser.add_argument('--n_thread', type=int, 
                         help='Number of threads to use in multiprocessing pool.')
-    
+    parser.add_argument('--dji_gps_source', type=pathlib.Path,
+                        help='Instead of using GPS data extracted from the image to get latitude and longitude, use GPS data extracted from a series of DJI camera images')
+
+
     elevation_group = parser.add_mutually_exclusive_group(required=True)
     elevation_group.add_argument('--height', type=int, 
                         help='Average distance between subject and drone during flight, in integer meters. For more accurate positioning over non-level terrain when GPS is available, use --heightmap option. When terrain is fairly level and GPS is available, the --elevation option can also be used.')
@@ -193,11 +245,16 @@ if __name__ == "__main__":
     input_directory = args.input
     output_directory = args.output
 
+    gps_source = None
+    if args.dji_gps_source:
+        gps_table = load_gps_table(args.dji_gps_source, source_type='dji')
+        gps_source = gps_table
+
     setup_dirs(input_directory, output_directory, args.yes)
 
     filelist = os.listdir(input_directory)
 
-    process_partial = partial(process_flir_image, input_directory, output_directory, height_source)
+    process_partial = partial(process_flir_image, input_directory, output_directory, height_source, gps_source)
     if args.debug:
         for file in filelist:
             process_partial(file)
